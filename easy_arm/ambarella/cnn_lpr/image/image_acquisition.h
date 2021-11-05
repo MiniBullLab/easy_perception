7bb373aff2bf00ff9acfa09452cb43eb14398e19
lpj0822 <1014153254@qq.com>
2021-11-05 18:52:46 +0800

    lipeijie update lpr

--- easy_arm/ambarella/cnn_lpr/image/image_acquisition.h
+++ easy_arm/ambarella/cnn_lpr/image/image_acquisition.h
@@ -1,5 +1,29 @@
 #ifndef _IMAGE_ACQUISITION_H_
 #define _IMAGE_ACQUISITION_H_
 
+#include <vector>
+#include <pthread.h>
+
+#include <glog/logging.h>
+#include <glog/raw_logging.h>
+
+//opencv
+#include <opencv2/core.hpp>
+
+class ImageAcquisition
+{
+public:
+
+    ImageAcquisition();
+    ~ImageAcquisition();
+
+    int open_camera();
+
+    int start();
+    int stop();
+
+private:
+    pthread_t pthread_id;
+};
 
 #endif // _IMAGE_ACQUISITION_H_
